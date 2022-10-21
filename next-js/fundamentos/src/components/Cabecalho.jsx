5cf6c4f5588c4bf913e05c7d4d5de8e17ec57937
Jonas-Grossi <jonasgrossicc@gmail.com>
2022-10-21 12:51:35 -0300

    Next.js

---
+++ next-js/fundamentos/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+export default function Cabecalho(props){
+
+    console.log(props.titulo)
+    return(
+        <header>
+            <h1>{props.titulo}</h1>
+        </header>
+
+
+    )
+}
---
+++ next-js/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,14 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+    return (
+        <div>
+
+            <Cabecalho titulo = "Next.js  & React"  />
+            <Cabecalho titulo = "Aprende Next na prática" />
+
+        </div>
+    )
+
+
+}
